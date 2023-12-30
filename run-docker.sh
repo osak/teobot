d6433679addecc5eff9604673de9e795d90f0622
Osamu Koga <github@osak.jp>
2023-12-31 07:00:26 +0900

    remote unnecessary mounting

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -v "$(pwd)/.env.docker:/opt/teobot/.env" -v "$(pwd)/data:/opt/teobot/data" --name teobot teobot:latest
+docker run -d -v "$(pwd)/data:/opt/teobot/data" --name teobot teobot:latest

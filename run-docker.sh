adbe2d0e2a70a45b4a73ef65887866c89f2aea04
Osamu Koga <github@osak.jp>
2025-04-28 01:36:35 +0900

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --restart always -d -v "$(pwd)/data:/opt/teobot/data" --name teobot teobot:latest
+docker run --restart always -d -v "$(pwd)/data:/opt/teobot/data" -v "$(pwd)/tmp:/opt/teobot/tmp" --name teobot teobot:latest
